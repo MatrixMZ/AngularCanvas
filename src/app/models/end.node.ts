89c17c871e4896fee0b4661613960e0b9f4cfe88
Mateusz Ziobrowski <matrix.ziobrowski@gmail.com>
2021-04-21 13:07:51 +0100

    End node update

--- src/app/models/end.node.ts
+++ src/app/models/end.node.ts
@@ -16,8 +16,15 @@ export class EndNode implements Node {
   handleMouseClick(mousePosition: Vector): void { }
 
   draw(ctx: CanvasRenderingContext2D): void {
-    circle(ctx, this.position, this.radius);
-    label(ctx, this.title, this.position, 10);
+    const height = 150;
+    const width = 200;
+    ctx.beginPath();
+    ctx.moveTo(this.position.x , this.position.y);
+    ctx.lineTo(this.position.x + width / 2, this.position.y + height);
+    ctx.lineTo(this.position.x - width / 2, this.position.y + height);
+    ctx.fillStyle = 'teal';
+    ctx.fill();
+    label(ctx, this.title, this.position.clone().add(new Vector(0, height / 2 + 20)), 10);
   }
 
 }
